1e4016fb8d6711bda21492c6131b6d0ba95fd87d
Wei(William) Wang <william.wang.uncharted@gmail.com>
2026-04-14 17:05:11 -0500

    fix banner1.

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,11 @@
 export default function Banner() {
+  const bannerImageUrl = `${import.meta.env.BASE_URL}images/banner.png`;
+
   return (
     <div
       style={{
         height: "300px",
-        backgroundImage: "url('./public/images/banner.png')",
+        backgroundImage: `url('${bannerImageUrl}')`,
         backgroundSize: "cover",
         color: "#fff",
         display: "flex",
